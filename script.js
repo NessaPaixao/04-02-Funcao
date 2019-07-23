fea4028dbf5275abda75d0a05f86ff31639dfa1d
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-07-23 17:27:22 -0300

    Testar funcoes

---
+++ script.js
@@ -0,0 +1,21 @@
+function multiplicar (num1, num2, num3){
+    console.log(num1 * num2 * num3);
+}
+function dividir(num1, num2, num3){
+    console.log(num1 / num2 / num3);
+}
+function somar(num1, num2, num3){
+    console.log(num1 + num2 + num3);
+}
+function subtrair(num1, num2, num3){
+    console.log(num1 - num2 - num3);
+}
+function somarSubtrair(num1, num2, num3){
+    console.log(num1 + num2 - num3);
+}
+
+multiplicar(2, 5, 12)
+dividir(200, 2, 5)
+subtrair(400, 234 ,54)
+somar(78, 242, 464)
+somarSubtrair(534,320, 712)
